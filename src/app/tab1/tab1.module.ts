b7a616d15df5dc1208343bb189fc70da6610b219
Vitória Ketillin Fusco <vitoriaketillin@hotmail.com>
2021-09-10 20:34:24 -0300

    v1.1

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,7 +1,7 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
@@ -11,6 +11,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     Tab1PageRoutingModule
   ],
   declarations: [Tab1Page]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//Importação para utilizar formulários do Angular
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +10,18 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  formCadastro: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {
+    //Montar a estrutura do formulário
+    this.formCadastro = formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      cpf:['', Validators.compose([Validators.required])],
+      dataNascimento:['', Validators.required],
+      senha:['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+      confirmaSenha:['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+    });
+  }
 
 }
